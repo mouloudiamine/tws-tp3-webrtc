542164af7148daac7d6e02bc36873d0865796666
Mouloudi Amine <dm_mouloudi@esi.dz>
2019-09-20 16:24:13 +0200

    move les @babel/... from dependencies to devDependencies

--- client/components/Content/index.jsx
+++ client/components/Content/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 export default class Content extends React.Component{
     render() {
-        return(<h1>this is Content</h1>)
+        return(<h1>TP1-TIW8- MOULOUDI Mohamed Amine</h1>)
     }
 }
