3c7459f807827de8fe926da76f279452382b59ae
Mouloudi Amine <dm_mouloudi@esi.dz>
2019-12-13 01:36:58 +0100

    partie 1 : TP3.1 WebRTC et vidéo en local

--- serveur/index.js
+++ serveur/index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const path = require('path');
+
+const DIST_DIR = path.join(__dirname, '../dist');
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
 const app = express();
 const port = process.env.PORT || 3000;
 const mockResponse = {
   foo: 'bar',
-  bar: 'foo'
-
+  bar: 'foo',
 };
-const DIST_DIR = path.join(__dirname, '../dist');
-const HTML_FILE = path.join(DIST_DIR, 'index.html');
-app.use(express.static(DIST_DIR));
-
 app.get('/api', (req, res) => {
   res.send(mockResponse);
 });
+
 app.get('/', (req, res) => {
- res.status(200).sendFile(HTML_FILE);
+  res.status(200).sendFile(HTML_FILE);
 });
-app.listen(port, function () {
- console.log('App listening on port: ' + port);
+
+app.listen(port, () => {
+  console.log(`App listening on port: ${port}`);
 });
+
+app.use(express.static(DIST_DIR));
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,69 @@
 const path = require('path');
 
 module.exports = {
-  entry: './serveur/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        }
-      ]
-    }
-  },
-  node: {
-    net: 'empty',
-    tls: 'empty',
-    dns: 'empty',
-    fs: 'empty'
-  },
+    entry: './serveur/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
 
+    node: {
+        fs: "empty",
+        net: 'empty',
+        tls: 'empty',
+        dns: 'empty'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader","eslint-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: "file-loader",
+                options: { name: '/static/[name].[ext]' }
+            }
+        ]
+    }
 };
 
+
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./client/index.html",
-  filename: "./index.html"
+    template: "./src/index.html",
+    filename: "./index.html"
 });
+
 module.exports = {
-  entry: "./client/index.jsx",
-  output: { // NEW
-    path: path.join(__dirname, 'dist'),
-    filename: "[name].js"
-  }, // NEW Ends
-  plugins: [htmlPlugin],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: "file-loader",
-        options: { name: '/static/[name].[ext]' }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    entry: "./src/index.js",
+    output: { // NEW
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js"
+    }, // NEW Ends
+    plugins: [htmlPlugin],
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: "file-loader",
+                options: { name: '/static/[name].[ext]' }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
         ]
-      }
-    ]
-  }
+    }
 };
